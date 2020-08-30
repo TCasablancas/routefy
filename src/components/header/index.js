b4d0adfda4555fc8ca3bd5282f378a4f18e0cc19
Thiago Silva <thyago_casablancas@hotmail.com>
2020-08-30 19:13:42 -0300

    Creating layout files
    Putting some layout files. And also the first db code lines.

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '../../styles/container';
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Routefy</h1>
+        </Container>
+    )
+}
---
+++ src/styles/container.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 60px;
+    padding: 0 30px;
+    background: #3D59F2;
+    font-size: 11px;
+    color: #fff;
+    display: flex;
+    align-items: center;
+`;
